65138ba148c25f7b603b85d61cc74c4f4e024794
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2023-11-16 14:53:25 -0500

    Player movement and placeholder sprite

---
+++ Player/Player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var sprite : Sprite2D = $Sprite2D
+
+const SPEED = 200.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	velocity = direction * SPEED
+
+	move_and_slide()
